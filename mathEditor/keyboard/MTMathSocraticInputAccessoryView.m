6fd938a24f3aa0131dd9759188ebc5d9a1ac5013
jdolecki <jakub.dolecki2@gmail.com>
2016-08-24 14:34:17 -0400

    Removes NSLogs

--- mathEditor/keyboard/MTMathSocraticInputAccessoryView.m
+++ mathEditor/keyboard/MTMathSocraticInputAccessoryView.m
@@ -25,17 +25,14 @@
 }
 
 - (IBAction)searchButtonTapped:(id)sender {
-    NSLog(@"Search button tapped");
     [self.delegate inputAccessoryViewSearchButtonTapped:self];
 }
 
 - (IBAction)deleteButtonTapped:(id)sender {
-    NSLog(@"Delete button tapped");
     [self.delegate inputAccessoryViewDeleteButtonTapped:self];
 }
 
 - (IBAction)clearButtonTapped:(id)sender {
-    NSLog(@"Clear button tapped");
     [self.delegate inputAccessoryViewClearButtonTapped:self];
 }
 
